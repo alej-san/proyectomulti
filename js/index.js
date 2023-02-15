4df43046eb15b258908abc48715df471ec1d4bb1
Iker-hub <ikergargu@gmail.com>
2023-02-15 12:35:05 +0100

    Upload files

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-new Chart(document.getElementById('graphicsChart'), {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: { beginAtZero: true }
-            }
-        }
-    }
-);
+new Chart(document.getElementById("graphicsChart"), {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: { beginAtZero: true },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  },
+});
