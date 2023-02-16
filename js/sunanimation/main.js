d5889da7bee10dcd55d318c7d38aebaa33ab8688
Iker-hub <ikergargu@gmail.com>
2023-02-16 14:12:33 +0100

    Upload files

--- js/sunanimation/main.js
+++ js/sunanimation/main.js
@@ -1,12 +1,23 @@
-var game = new Phaser.Game(1500, 600, Phaser.CANVAS, "phaser-example", {
-  preload: preload,
-  create: create,
-  update: update,
-});
+let sunAnimationContainer = document.querySelector("#sunanimation-container");
+
+var game = new Phaser.Game(
+  sunAnimationContainer.offsetWidth,
+  600,
+  Phaser.CANVAS,
+  "sunanimation-container",
+  {
+    preload: preload,
+    create: create,
+    update: update,
+  }
+);
 
 function preload() {
   game.load.image("ship", "./js/sunanimation/ship.png");
   game.load.image("sun", "./js/sunanimation/sun.png");
+
+  this.gameWidth = this.game.width;
+  this.gameHeight = this.game.height;
 }
 
 var ship;
@@ -16,38 +27,31 @@ var cursors;
 function create() {
   game.stage.backgroundColor = "#001255";
 
-  ship = game.add.sprite(400, 300, "ship");
+  ship = game.add.sprite(this.gameWidth / 2, this.gameHeight / 2, "ship");
   ship.anchor.setTo(0.5, 0.5);
 
   game.physics.arcade.enable(ship);
 
-  orb = game.add.sprite(400, 300, "sun");
+  orb = game.add.sprite(this.gameWidth / 2, this.gameHeight / 2, "sun");
   orb.anchor.setTo(0.5);
   orb.pivot.x = 100;
-
-  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  ship.body.velocity.x = 0;
-  ship.body.velocity.y = 0;
+  orb.rotation += 0.02;
 
-  if (cursors.left.isDown) {
-    ship.body.velocity.x = -300;
-  } else if (cursors.right.isDown) {
-    ship.body.velocity.x = 300;
-  }
-
-  if (cursors.up.isDown) {
-    ship.body.velocity.y = -300;
-  } else if (cursors.down.isDown) {
-    ship.body.velocity.y = 300;
-  }
-
-  orb.rotation += 0.05;
+  resizeAnimation(this.game);
 }
 
 function preRender() {
   orb.x = ship.x;
   orb.y = ship.y;
+}
+
+function resizeAnimation(game) {
+  game.scale.setGameSize(sunAnimationContainer.offsetWidth, 600);
+  ship.x = game.width / 2;
+  ship.y = game.height / 2;
+  orb.x = game.width / 2;
+  orb.y = game.height / 2;
 }
